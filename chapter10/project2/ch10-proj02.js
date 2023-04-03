f4808e482ecd019625a9abb66c80c1d4edf85937
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2023-04-03 00:07:09 -0500

    Chapter 9 proj 2

--- chapter10/project2/ch10-proj02.js
+++ chapter10/project2/ch10-proj02.js
@@ -1,7 +1,5 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
-
 	
 	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
 
@@ -20,4 +18,135 @@ document.addEventListener("DOMContentLoaded", function() {
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
+
+	let jsonData;
+	let playList = document.querySelector("#playList")
+	playList.addEventListener("change", (e) => {
+
+		let data = fetch(url + "?name=" + playList.value).then (function(response){
+			return response.json()
+		}).then(function (data){
+			dataManipulator(data)
+			jsonData = data
+		})
+	})
+
+	let but = document.querySelector("#btnHighlight")
+	but.addEventListener("click", (e) =>{
+		highlight(jsonData)
+	})
+
 });
+
+function dataManipulator(data) {
+	// Act List
+	let actList = document.querySelector("#actList")
+	actList.innerHTML = ""
+
+	for(let num = 0; num < data.acts.length; num++) {
+		actList.innerHTML += "<option value=" + num + ">" + data.acts[num].name + "</option>"
+	}
+
+	actList.addEventListener("change", (e) => {
+		updateSceneList(data, actList)
+	})
+
+	updateSceneList(data, actList)
+}
+
+function updateSceneList(data, actList){
+	// Scene List
+	let sceneList = document.querySelector("#sceneList")
+	sceneList.innerHTML = ""
+
+	for(let num = 0; num < data.acts[actList.value].scenes.length; num++) {
+		sceneList.innerHTML += "<option value=" + num + ">" + data.acts[actList.value].scenes[num].name + "</option>"
+	}
+
+	sceneList.addEventListener("change", (e) => {
+		// Play Here
+		updatePlayHere(data, actList, sceneList)
+	})
+
+	updatePlayHere(data, actList, sceneList, "0", "")
+	updatePlayerList(data, actList, sceneList)
+}
+
+function updatePlayHere(data, actList, sceneList, actor, input) {
+	// Play Here
+	let playHere = document.querySelector("#playHere")
+	let currentAct = data.acts[actList.value];
+	let currentScene = data.acts[actList.value].scenes[sceneList.value]
+
+	playHere.innerHTML = "<h2>" + data.title + "</h2>"
+	playHere.innerHTML += "<article id='actHere'> <h3>" + currentAct.name + "</h3> <div id='sceneHere'>" +
+		" <h4> " + currentScene.name + " </h4> <p class='title'>" + currentScene.title + "</p> <p class='direction'> " + currentScene.stageDirection + "</p>"
+
+	for(let num = 0; num < currentScene.speeches.length; num++){
+		let cur = currentScene.speeches[num]
+
+		if(actor === "0")
+			inputSpeech(playHere, cur, input)
+		else {
+			if(actor === cur.speaker)
+				inputSpeech(playHere, cur, input)
+		}
+
+		playHere.innerHTML += "</div>"
+	}
+
+	playHere.innerHTML +=  "</div> </article>"
+}
+
+function inputSpeech(playHere, cur, input){
+	playHere.innerHTML += "<div class='speech'> <span>" + cur.speaker + "</span>"
+
+	for(let numLines = 0; numLines < cur.lines.length; numLines++){
+		if(input !== "")
+		{
+			playHere.innerHTML += checkLine(cur.lines[numLines], input)
+		}
+		else
+		{
+			playHere.innerHTML += "<p>" + cur.lines[numLines] + "</p>"
+		}
+	}
+}
+
+function updatePlayerList(data, actList, sceneList) {
+	let playerList = document.querySelector("#playerList")
+	let currentAct = data.acts[actList.value];
+	let currentScene = data.acts[actList.value].scenes[sceneList.value]
+
+	playerList.innerHTML = "<option value=0>All Players</option>"
+
+	const uniqueCharacters = [];
+	for(let num = 0; num < currentScene.speeches.length; num++){
+		let speaker = currentScene.speeches[num].speaker
+
+		if(uniqueCharacters.includes(speaker)) continue
+
+		uniqueCharacters[uniqueCharacters.length] = speaker
+		playerList.innerHTML += "<option value=" + speaker + ">" + speaker + "</option>"
+	}
+}
+
+function highlight(data) {
+	let actor = document.querySelector("#playerList").value
+	let input = document.querySelector("#txtHighlight").value
+
+	updatePlayHere(data, document.querySelector("#actList"), document.querySelector("#sceneList"), actor, input)
+}
+
+function checkLine(line, condition) {
+
+	for(let i = 0; i < line.length - condition.length; i++){
+		let word = line.slice(i, i + condition.length)
+
+		if(word === condition){
+			return "<p style='background-color: yellow'>" + line + "</p>"
+		}
+	}
+
+	return "<p>" + line + "</p>"
+}
--- chapter10/project2/play-module.js
+++ chapter10/project2/play-module.js
@@ -1,2 +1,106 @@
-/* In this module, create three classes: Play, Act, and Scene. */
+class Play {
+    constructor(playData) {
 
+        this.acts = [];
+        this.players = [];
+
+        this.title = playData.title;
+        this.short = playData.short;
+
+        playData.persona.forEach( p => {  this.players.push(p)});
+
+        playData.acts.forEach( a => {
+            this.acts.push( new Act(a) );
+        });
+    }
+
+    output(playContainer, actContainer, sceneContainer) {
+        playContainer.textContent = this.title;
+        this.acts[0].output(actContainer, sceneContainer);
+    }
+
+}
+
+class Act {
+
+    constructor(actData) {
+        this.name = actData.name;
+        this.scenes = [];
+        actData.scenes.forEach( s => {
+            this.scenes.push( new Scene(s.name, s.title, s.stageDirection, s.speeches) );
+        })
+
+    }
+
+    output(actContainer, sceneContainer) {
+        actContainer.textContent = this.name;
+        this.scenes[0].output(sceneContainer);
+    }
+
+}
+
+
+class Scene {
+
+    constructor(name,title,direction,speeches) {
+        this.name = name;
+        this.title = title;
+        this.direction = direction;
+        this.speeches = speeches;
+    }
+
+
+    output(container, search, player) {
+        container.innerHTML = "";
+        const h4 = document.createElement('h4');
+        h4.textContent = this.name;
+        container.appendChild(h4);
+        const pTitle = document.createElement('p');
+        pTitle.classList.add('title');
+        pTitle.textContent = this.title;
+        container.appendChild(pTitle);
+        const pDirection = document.createElement('p');
+        pDirection.classList.add('direction');
+        pDirection.textContent = this.direction;
+        container.appendChild(pDirection);
+
+        this.outputSpeeches(container, search, player);
+
+    }
+
+    outputSpeeches(container, search, player) {
+        const divs = this.speeches.map( sp => {
+
+
+            const div = document.createElement('div');
+            div.classList.add('speech');
+            if (player && sp.speaker != player && player != 0) return div;
+            const span = document.createElement('span');
+            span.textContent = sp.speaker;
+            div.appendChild(span);
+            const lines = sp.lines.map( line => {
+                const p = document.createElement('p');
+                if (search) {
+                    const newLine = line.replace(new RegExp(search, "i"),`<b>${search}</b>`);
+                    p.innerHTML = newLine;
+                } else {
+                    p.textContent = line;
+                }
+                return p;
+            } );
+            lines.forEach( line => { div.appendChild(line) } );
+            if (sp.stagedir) {
+                const em = document.createElement('em');
+                em.textContent = sp.stagedir;
+                div.appendChild(em);
+            }
+            return div;
+        });
+
+        divs.forEach( div => { container.appendChild(div) } );
+    }
+}
+
+
+
+export { Play };
